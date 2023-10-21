d6b7753c89d81317feeb92fa25352f468c184e5c
lucasemanuel345 <leprodrigues@sga.pucminas.br>
2023-10-21 14:17:19 -0300

    Adicionar arquivos de projeto.

---
+++ mf-dev-backend-toyota/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mf_dev_backend_toyota.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
